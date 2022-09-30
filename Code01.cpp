4ef49424487f89f5845c908130f987da6cb873cd
breadan alaa <109834067+breadan@users.noreply.github.com>
2022-09-30 09:34:10 +0200

    Add files via upload
    prime with function

---
+++ Code01.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+int prime(int num)
+{
+	int div = num / 2;
+	for (int i = 2; i <= div; i++)
+	{
+		if (num % i == 0)
+			return -1;
+		break;
+	}
+	return num;
+}
+
+int main()
+{
+	int a, b ;
+	int count = 0;
+
+	cin >> a >> b;
+	while (a <= b)
+	{
+		a *= 3;
+		b *= 2;
+		count++;
+	}		cout << count;
+
+
+
+
+
+	return 0;
+}
+
